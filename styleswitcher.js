e48a910e6343f31f3b856b96a63098cca4f2f1bb
Asmara <asmaraviantielgracia@gmail.com>
2023-12-17 15:55:25 +0100

    Uploaded website files

---
+++ styleswitcher.js
@@ -0,0 +1,7 @@
+function toggleStyle() {
+  console.log('Button clicked!'); // Add this line for debugging
+  var body = document.body;
+  body.classList.toggle('high-contrast');
+  body.classList.toggle('larger-text');
+}
+
